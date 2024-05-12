546c0b48fb9dfef6aecca798e636c0bd043d34fb
rubygrv19 <rubygrv19@gmail.com>
2024-05-12 23:47:57 +0700

    - Add Ionic - Add Capacitor

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: false,
   modules: [
     '@nuxtjs/tailwindcss',
-    '@nuxtjs/google-fonts'
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/ionic'
   ],
   googleFonts: {
 		families: {
